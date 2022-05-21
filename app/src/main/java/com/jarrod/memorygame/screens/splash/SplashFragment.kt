5945a5f5da3c699f5dffa85b07e3a3c9d767157c
Joel Jarro <joeljarro2@gmail.com>
2022-05-20 23:07:50 -0400

    base flip cards

--- app/src/main/java/com/jarrod/memorygame/screens/splash/SplashFragment.kt
+++ app/src/main/java/com/jarrod/memorygame/screens/splash/SplashFragment.kt
@@ -1,11 +1,14 @@
 package com.jarrod.memorygame.screens.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.jarrod.memorygame.R
 import com.jarrod.memorygame.databinding.FragmentSplashBinding
 
@@ -31,5 +34,14 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            navController = Navigation.findNavController(view)
+
+            navController.navigate(R.id.action_splashFragment_to_gameFragment)
+        }, 2000)
+    }
 
 }
